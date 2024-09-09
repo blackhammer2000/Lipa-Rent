09e1b7b9c1f85599b39e72b7c83c114f5f1ae24e
wawerusamuel <waweruzamuel@gmail.com>
2024-09-09 19:01:56 +0300

    created the accesstoken generator...

---
+++ SERVER/middleware/tokens/accessToken.js
@@ -0,0 +1,17 @@
+const { sign } = require("jsonwebtoken");
+require("dotenv").config();
+
+const signAccessToken = (userData) => {
+  return new Promise((resolve, reject) => {
+    const token = sign(userData, process.env.MY_SECRET_KEY, {
+      expiresIn: "1h",
+      issuer: "liparent inc.",
+      audience: `${userData}`,
+    });
+
+    if (!token) reject(token);
+    resolve(token);
+  });
+};
+
+module.exports = { signAccessToken };
