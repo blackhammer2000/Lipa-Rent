0ab85e52f12e0cb3f55d769250681de5120397f5
wawerusamuel <waweruzamuel@gmail.com>
2024-09-09 17:54:04 +0300

    working on the server...

--- SERVER/server.js
+++ SERVER/server.js
@@ -4,21 +4,24 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const PORT = process.env.PORT || 8080;
-const { connect } = require("mongoose");
+const Mongoose = require("mongoose");
 
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:5501" || "http://127.0.0.1:5501",
+    origin: "http://localhost:5502" || "http://127.0.0.1:5502",
   })
 );
 
 // =
 
 app.listen(PORT, () => {
-  console.log(`server running. on PORT ${PORT}.`);
+  console.log(`server running on PORT ${PORT}.`);
 });
-
-connect(process.env.DB_CONNECTION, () => {
-  console.log("connected to database.");
-});
+try {
+  Mongoose.connect(process.env.DB_CONNECTION).then(() => {
+    console.log("connected to database.");
+  });
+} catch (error) {
+  if (error) console.log(`Error connecting to database: ${error.message}`);
+}
