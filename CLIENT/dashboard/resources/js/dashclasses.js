3f48c78d14e057eea5190a2a84ac16958b9b650b
wawerusamuel <waweruzamuel@gmail.com>
2024-09-13 12:24:59 +0300

    Checking to see if the properties are being properly read from the database...

--- CLIENT/dashboard/resources/js/dashclasses.js
+++ CLIENT/dashboard/resources/js/dashclasses.js
@@ -1,7 +1,35 @@
-class Store extends StoreUtilities {
-  static readAllPropertiesOwned(accessToken) {}
+class Store {
+  static async readAllPropertiesOwned(accessToken) {
+    if (!accessToken) location?.assign("../../../login/login.html");
+
+    const getAllPropertiesData = await fetch(
+      "http://localhost:4000/api/user/landlord/read/allProperties",
+      {
+        method: "GET",
+        mode: "cors",
+        headers: {
+          "Content-Type": "application/json",
+          token: accessToken,
+        },
+      }
+    );
+
+    const { allProperties, error } = await getAllPropertiesData?.json();
+
+    if (error && error?.toLowerCase() !== "session expired") {
+      // UserInterface?.showALertMessage(error, "danger");
+      alert(error);
+
+      return;
+    }
+
+    if (error && error?.toLowerCase() === "session expired")
+      location?.assign("../TEXTBOOK LOG IN/textbooklogin.html");
+
+    if (allProperties) return allProperties;
+  }
 }
 
-class UserInterface extends UserinterfaceUtilities {
+class UserInterface {
   static renderProperties() {}
 }
