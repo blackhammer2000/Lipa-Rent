be27325aa8005e7c9f9be4816126fe30263a407d
wawerusamuel <waweruzamuel@gmail.com>
2024-09-13 12:46:49 +0300

    done with the endpoint controller...

--- SERVER/routes/users/controllers/getControllers.js
+++ SERVER/routes/users/controllers/getControllers.js
@@ -5,7 +5,21 @@ const { Property } = require("../../../middleware/models/Property");
 const getControllers = {
   readAllProperties: async (req, res) => {
     try {
-    } catch (err) {}
+      const { id } = req.body;
+
+      if (!id)
+        throw new Error("Unauthorized action, not a user or not logged in.");
+
+      const allProperties = await Property.findOne({ landlordID: id });
+
+      if (!allProperties) throw new Error(allProperties);
+
+      const { propertiesOwned } = allProperties;
+
+      res.status(200).json({ propertiesOwned });
+    } catch (err) {
+      if (err.message) res.status(400).json({ error: err.message });
+    }
   },
 };
 
