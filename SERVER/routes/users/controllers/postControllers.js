72afc9fa4c777316fbfa59963016e6334a0138f0
wawerusamuel <waweruzamuel@gmail.com>
2024-09-16 21:52:46 +0300

    working on the create tenant method...

--- SERVER/routes/users/controllers/postControllers.js
+++ SERVER/routes/users/controllers/postControllers.js
@@ -586,14 +586,64 @@ const post_controllers = {
       //   if (!req.body.id) throw new Error("Unknown user...");
       if (!req.body.propertyId) throw new Error("provide a valid property ID.");
       if (!req.body.propertyNo) throw new Error("provide a valid property NO.");
-      if (!req.body.roomId) throw new Error("provide a valid room NO.");
+      if (!req.body.roomId) throw new Error("provide a valid room ID.");
       if (!req.body.tenant) throw new Error("provide a valid tenant.");
 
       const { id, propertyId, propertyNo, roomId, tenant } = req.body;
 
-      const allPropertiesAndRoomsDB = [...TenantsDB.propertiesTenants];
+      const allPropertiesAndRoomsAndTenantsDB = [
+        ...TenantsDB.propertiesTenants,
+      ];
 
-      if (!allPropertiesAndRoomsDB) throw new Error("no data found");
+      if (!allPropertiesAndRoomsAndTenantsDB) throw new Error("no data found");
+
+      const checkIfPropertyIdIsRegistered =
+        allPropertiesAndRoomsAndTenantsDB[0][propertyId];
+
+      if (
+        !checkIfPropertyIdIsRegistered ||
+        (!checkIfPropertyIdIsRegistered &&
+          checkIfPropertyIdIsRegistered.propertyNumber !== propertyNo)
+      ) {
+        if (
+          !checkIfPropertyIdIsRegistered &&
+          checkIfPropertyIdIsRegistered.propertyNumber !== propertyNo
+        )
+          throw new Error(
+            "Property with the given property Id and  property number has not been registered."
+          );
+
+        if (checkIfPropertyIdIsRegistered)
+          throw new Error(
+            "Property with the given property Id has not been registered."
+          );
+      }
+
+      const propertyTenants = checkIfPropertyIdIsRegistered?.tenants;
+
+      if (!propertyTenants)
+        throw new Error("No tenants have been added to this property.");
+
+      const checkIfRoomIdIsRegistered = propertyTenants[roomId];
+
+      if (
+        !checkIfRoomIdIsRegistered ||
+        (!checkIfRoomIdIsRegistered &&
+          checkIfRoomIdIsRegistered.roomNumber !== newRoom.roomNo)
+      ) {
+        if (
+          !checkIfRoomIdIsRegistered &&
+          checkIfRoomIdIsRegistered.roomNumber !== newRoom.roomNo
+        )
+          throw new Error(
+            "Room with the given ID and number has not been registered."
+          );
+
+        if (checkIfRoomIdIsRegistered)
+          throw new Error(
+            "Room with the given ID has not been registered in the tenants database."
+          );
+      }
     } catch (err) {
       if (err.message) res.status(400).json({ error: err.message });
     }
