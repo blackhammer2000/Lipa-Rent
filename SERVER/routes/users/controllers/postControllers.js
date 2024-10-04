3fcc4e1aed9ef3d45a7a90f42b8f8a4f98a7773b
wawerusamuel <waweruzamuel@gmail.com>
2024-10-04 15:13:57 +0300

    successfully read all tenants for a single room...

--- SERVER/routes/users/controllers/postControllers.js
+++ SERVER/routes/users/controllers/postControllers.js
@@ -938,21 +938,23 @@ const post_controllers = {
   //*  }
   readAllTenantsInRoomOnProperty: async (req, res) => {
     try {
-      // if (!req.body.id) throw new Error("Unknown user...");
+      if (!req.body.id) throw new Error("Unknown user...");
       if (!req.body.propertyId) throw new Error("provide a valid property ID.");
       if (!req.body.propertyNo) throw new Error("provide a valid property NO.");
       if (!req.body.roomId) throw new Error("provide a valid room ID.");
 
       const { id, propertyId, propertyNo, roomId } = req?.body;
 
-      const allPropertiesAndRoomsAndTenantsDB = [
-        ...TenantsDB.propertiesTenants,
-      ];
+      if (!isValid(id))
+        throw new Error("ID provided is not a valid document Id.");
 
-      if (!allPropertiesAndRoomsAndTenantsDB) throw new Error("no data found");
+      const tenantsDocument = await Tenant.findOne({ ownerID: id });
 
-      const checkIfPropertyIdIsRegistered =
-        allPropertiesAndRoomsAndTenantsDB[0][propertyId];
+      if (!tenantsDocument) throw new Error(tenantsDocument);
+
+      const { tenants } = tenantsDocument;
+
+      const checkIfPropertyIdIsRegistered = tenants[0][propertyId];
 
       if (
         !checkIfPropertyIdIsRegistered ||
@@ -979,6 +981,11 @@ const post_controllers = {
         throw new Error("No tenants have been added to this property.");
 
       const selectedRoomOnPropertyTenants = selectedPropertyTenants[roomId];
+
+      if (!selectedRoomOnPropertyTenants)
+        throw new Error(
+          `No room with the roomID: ${roomId} been added to this property.`
+        );
 
       res.status(200).json({ selectedRoomOnPropertyTenants });
     } catch (err) {
