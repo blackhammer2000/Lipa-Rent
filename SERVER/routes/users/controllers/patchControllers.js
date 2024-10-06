850879b986597c0b63010a923c6fccd7a32297d0
wawerusamuel <waweruzamuel@gmail.com>
2024-10-06 11:16:39 +0300

    finished working on the edit property method...

--- SERVER/routes/users/controllers/patchControllers.js
+++ SERVER/routes/users/controllers/patchControllers.js
@@ -15,11 +15,13 @@ const patchControllers = {
     try {
       if (!req.body.id)
         throw new Error("Unauthorized action, not a user or not logged in.");
-
+      if (!req.body.propertyNo)
+        throw new Error("Provide a valid property number.");
+      if (!req.body.propertyId) throw new Error("provide a valid property Id.");
       if (!req.body.editedProperty)
         throw new Error("provide a valid property.");
 
-      const { id, editedProperty } = req.body;
+      const { id, propertyNo, propertyId, editedProperty } = req.body;
 
       if (!isValid(id))
         throw new Error("ID provided is not a valid document Id.");
@@ -29,8 +31,44 @@ const patchControllers = {
       if (!ownerPropertiesDocument) throw new Error(ownerPropertiesDocument);
 
       const { propertiesOwned } = ownerPropertiesDocument;
-    } catch (err) {}
+
+      const checkIfPropertyIdIsRegistered = propertiesOwned[propertyId];
+
+      if (!checkIfPropertyIdIsRegistered)
+        throw new Error("Property with the given property ID was not found.");
+
+      if (checkIfPropertyIdIsRegistered.propertyNumber !== propertyNo)
+        throw new Error(
+          "The property number given does not match the number of the property saved in the database with the given ID."
+        );
+
+      propertiesOwned[0][checkIfPropertyIdIsRegistered.propertyId] = {
+        ...editedProperty,
+        propertyID: checkIfPropertyIdIsRegistered.propertyId,
+      };
+
+      const updateEditedProperty = await Property.updateOne(
+        { ownerID: id },
+        { $set: { propertiesOwned } }
+      );
+
+      if (!updateEditedProperty.acknowledged)
+        throw new Error("Error when updating the property in the database.");
+
+      res.status(200).json({
+        editedProperty: {
+          ...editedProperty,
+          propertyID: checkIfPropertyIdIsRegistered.propertyId,
+        },
+      });
+    } catch (err) {
+      if (err?.message)
+        res
+          .status(500)
+          .json({ error: err?.message, response_status: "danger" });
+    }
   },
+
   editPropertyDetails: async (req, res) => {},
   editRoomDetails: async (req, res) => {},
   editTenantDetails: async (req, res) => {},
