1fe869720fdaab530a0f2118b95ef6a629b3357b
wawerusamuel <waweruzamuel@gmail.com>
2024-10-06 11:02:13 +0300

    working on the patch property method...

--- SERVER/routes/users/controllers/patchControllers.js
+++ SERVER/routes/users/controllers/patchControllers.js
@@ -11,7 +11,26 @@ const { Rent } = require("../../../middleware/models/Rent");
 
 ///////*************************PATCHCONTROLLERS************************////////////////
 const patchControllers = {
-  editOwnerDetails: async (req, res) => {},
+  editOwnerDetails: async (req, res) => {
+    try {
+      if (!req.body.id)
+        throw new Error("Unauthorized action, not a user or not logged in.");
+
+      if (!req.body.editedProperty)
+        throw new Error("provide a valid property.");
+
+      const { id, editedProperty } = req.body;
+
+      if (!isValid(id))
+        throw new Error("ID provided is not a valid document Id.");
+
+      const ownerPropertiesDocument = await Property.findOne({ ownerID: id });
+
+      if (!ownerPropertiesDocument) throw new Error(ownerPropertiesDocument);
+
+      const { propertiesOwned } = ownerPropertiesDocument;
+    } catch (err) {}
+  },
   editPropertyDetails: async (req, res) => {},
   editRoomDetails: async (req, res) => {},
   editTenantDetails: async (req, res) => {},
