5cba442a02c61d3353704f5b59bbc847786df27d
wawerusamuel <waweruzamuel@gmail.com>
2024-10-10 17:05:01 +0300

    working on the delete controller methods...

---
+++ SERVER/routes/users/controllers/deleteControllers.js
@@ -0,0 +1,377 @@
+const {
+  ObjectId: { isValid },
+} = require("mongodb");
+
+const { Owner } = require("../../../middleware/models/Owner");
+const { Property } = require("../../../middleware/models/Property");
+const { Room } = require("../../../middleware/models/Room");
+const { Tenant } = require("../../../middleware/models/Tenant");
+const { Rent } = require("../../../middleware/models/Rent");
+
+///////*************************PATCHCONTROLLERS************************////////////////
+const patchControllers = {
+  editOwnerDetails: async (req, res) => {
+    try {
+      if (!req.body.id)
+        throw new Error("Unauthorized action, not a user or not logged in.");
+      if (!req.body.editedOwner)
+        throw new Error("Provide valid edited owner details.");
+
+      const { id, editedOwner } = req.body;
+
+      if (!isValid(id))
+        throw new Error("ID provided is not a valid document Id.");
+
+      const { name, nationalID } = editedOwner;
+
+      const ownerPropertiesDocument = await Owner.findOne({
+        _id: id.toString(),
+      });
+
+      if (!ownerPropertiesDocument) throw new Error(ownerPropertiesDocument);
+
+      const ownerUpdate = await Owner.updateOne(
+        { _id: id },
+        {
+          $set: { name, nationalID },
+        },
+        { new: true }
+      );
+
+      if (!ownerUpdate.acknowledged)
+        throw new Error(
+          "Error when updating the owner details in the database."
+        );
+
+      res.status(203).json({ editedOwner });
+    } catch (err) {
+      if (err?.message)
+        res
+          .status(500)
+          .json({ error: err?.message, response_status: "danger" });
+    }
+  },
+
+  editPropertyDetails: async (req, res) => {
+    try {
+      if (!req.body.id)
+        throw new Error("Unauthorized action, not a user or not logged in.");
+      if (!req.body.propertyNo)
+        throw new Error("Provide a valid property number.");
+      if (!req.body.propertyId) throw new Error("provide a valid property Id.");
+
+      const { id, propertyNo, propertyId } = req.body;
+      delete editedProperty.propertyID;
+
+      if (!isValid(id))
+        throw new Error("ID provided is not a valid document Id.");
+
+      const ownerPropertiesDocument = await Property.findOne({ ownerID: id });
+
+      if (!ownerPropertiesDocument) throw new Error(ownerPropertiesDocument);
+
+      const { propertiesOwned } = ownerPropertiesDocument;
+
+      const checkIfPropertyIdIsRegistered = propertiesOwned[0][propertyId];
+
+      if (!checkIfPropertyIdIsRegistered)
+        throw new Error("Property with the given property ID was not found.");
+
+      if (checkIfPropertyIdIsRegistered.propertyNumber !== propertyNo)
+        throw new Error(
+          "The property number given does not match the number of the property saved in the database with the given ID."
+        );
+
+      delete propertiesOwned[0][checkIfPropertyIdIsRegistered.propertyID];
+
+      const updateEditedProperty = await Property.updateOne(
+        { ownerID: id },
+        { $set: { propertiesOwned } }
+      );
+
+      if (!updateEditedProperty.acknowledged)
+        throw new Error("Error when updating the property in the database.");
+
+      res.status(200).json({
+        editedProperty: {
+          propertyID: checkIfPropertyIdIsRegistered.propertyID,
+          ...editedProperty,
+        },
+      });
+    } catch (err) {
+      if (err?.message)
+        res
+          .status(500)
+          .json({ error: err?.message, response_status: "danger" });
+    }
+  },
+
+  editRoomDetails: async (req, res) => {
+    try {
+      if (!req.body.id)
+        throw new Error("Unauthorized action, not a user or not logged in.");
+      if (!req.body.propertyId) throw new Error("provide a valid property Id.");
+      if (!req.body.propertyNo) throw new Error("provide a valid property Nd.");
+      if (!req.body.roomId) throw new Error("provide a valid room Id.");
+      if (!req.body.roomNo) throw new Error("provide a valid room No.");
+      if (!req.body.editedRoom)
+        throw new Error("provide a valid edited property details.");
+
+      const { id, propertyId, propertyNo, roomId, roomNo, editedRoom } =
+        req.body;
+
+      if (!isValid(id))
+        throw new Error("ID provided is not a valid document Id.");
+
+      const roomsDocument = await Room.findOne({ ownerID: id });
+
+      if (!roomsDocument) throw new Error(roomsDocument);
+
+      const { rooms } = roomsDocument;
+
+      const selectPropertyUsingPropertyID = rooms[0][propertyId];
+
+      if (!selectPropertyUsingPropertyID)
+        throw new Error(
+          "Selected propertyID Rooms are not found/not registered in the database."
+        );
+      if (selectPropertyUsingPropertyID.propertyNumber !== propertyNo)
+        throw new Error(
+          "Selected propertyNumber Rooms are not found/not registered in the database."
+        );
+
+      const propertyRooms = selectPropertyUsingPropertyID?.rooms;
+
+      const selectRoomInPropertyUsingRoomID = propertyRooms[roomId];
+
+      if (!selectRoomInPropertyUsingRoomID)
+        throw new Error(
+          "Selected room ID is not found/not registered in the property."
+        );
+      if (selectRoomInPropertyUsingRoomID.roomNumber !== roomNo)
+        throw new Error("Selected room ID and room number Room do not match.");
+
+      if (!Object.keys(selectRoomInPropertyUsingRoomID))
+        throw new Error("No rooms have been added to this property.");
+
+      // const newRoomDetails = {};
+
+      for (const key in editedRoom) {
+        if (key === "roomID") {
+          selectRoomInPropertyUsingRoomID[key] =
+            selectRoomInPropertyUsingRoomID[key];
+        } else {
+          selectRoomInPropertyUsingRoomID[key]
+            ? (selectRoomInPropertyUsingRoomID[key] = editedRoom[key])
+            : null;
+        }
+      }
+
+      delete rooms[0][propertyId].rooms[roomId];
+
+      const updateRooms = await Room.updateOne(
+        { ownerID: id },
+        {
+          $set: {
+            rooms,
+          },
+        }
+      );
+
+      if (updateRooms.acknowledged && updateRooms.modifiedCount)
+        res.status(200).json({
+          message: `Room with the Number: ${roomNo} and ID: ${roomId} has been successfuly deleted.`,
+        });
+    } catch (err) {
+      if (err.message) res.status(400).json({ error: err.message });
+    }
+  },
+
+  deleteTenantDetails: async (req, res) => {
+    try {
+      if (!req.body.id) throw new Error("Unknown user...please log in");
+      if (!req.body.propertyId) throw new Error("provide a valid property Id.");
+      if (!req.body.propertyNo) throw new Error("provide a valid property No.");
+      if (!req.body.roomId) throw new Error("provide a valid room Id.");
+      if (!req.body.tenantId) throw new Error("provide a valid tenant Id.");
+
+      const { id, propertyId, propertyNo, roomId, tenantId } = req?.body;
+
+      if (!isValid(id))
+        throw new Error("ID provided is not a valid document Id.");
+
+      const tenantsDocument = await Tenant.findOne({ ownerID: id });
+
+      if (!tenantsDocument) throw new Error(tenantsDocument);
+
+      const { tenants } = tenantsDocument;
+
+      const checkIfPropertyIdIsRegistered = tenants[0][propertyId];
+
+      if (
+        !checkIfPropertyIdIsRegistered ||
+        (!checkIfPropertyIdIsRegistered &&
+          checkIfPropertyIdIsRegistered?.propertyNumber !== propertyNo)
+      ) {
+        if (
+          !checkIfPropertyIdIsRegistered &&
+          checkIfPropertyIdIsRegistered?.propertyNumber !== propertyNo
+        )
+          throw new Error(
+            "Property with the given property Id and  property number has not been registered in the tenants database."
+          );
+
+        if (!checkIfPropertyIdIsRegistered)
+          throw new Error(
+            "Property with the given property Id has not been registered in the tenants database."
+          );
+      }
+
+      const selectedPropertyTenants = checkIfPropertyIdIsRegistered?.tenants;
+
+      if (!selectedPropertyTenants)
+        throw new Error("No tenants have been added to this property.");
+
+      const selectedRoomOnPropertyTenants = selectedPropertyTenants[roomId];
+
+      if (!selectedRoomOnPropertyTenants)
+        throw new Error("No tenants have been added to this room.");
+
+      const selectedTenantOnRoomOnProperty =
+        selectedRoomOnPropertyTenants[tenantId];
+
+      if (!selectedTenantOnRoomOnProperty)
+        throw new Error(
+          "No tenant with the given tenantId has been added to this room."
+        );
+
+      delete tenants[0][propertyId].tenants[roomId][tenantId];
+
+      const updateTenants = await Tenant.updateOne(
+        { ownerID: id },
+        {
+          $set: {
+            tenants,
+          },
+        }
+      );
+
+      if (updateTenants.acknowledged && updateTenants.modifiedCount)
+        res.status(200).json({
+          message: `Tenant with the Name: ${selectedTenantOnRoomOnProperty.name} and ID: ${tenantId} has been successfuly deleted.`,
+          selectedTenantOnRoomOnProperty,
+        });
+    } catch (err) {
+      if (err?.message) res.status(400).json({ error: err.message });
+    }
+  },
+
+  deleteRentPaymentDetails: async (req, res) => {
+    try {
+      if (!req.body.id) throw new Error("Unknown user...");
+      if (!req?.body.propertyId)
+        throw new Error("provide a valid property Id.");
+      if (!req?.body.propertyNo)
+        throw new Error("provide a valid property NO.");
+      if (!req?.body.roomId) throw new Error("provide a valid room Id.");
+      if (!req?.body.tenantId) throw new Error("provide a valid tenant Id.");
+      if (!req.body.paymentId) throw new Error("provide a valid payment Id.");
+      if (!req.body.editedRent)
+        throw new Error("provide a valid edited Rent doc.");
+
+      const {
+        id,
+        propertyId,
+        propertyNo,
+        roomId,
+        tenantId,
+        paymentId,
+        editedRent,
+      } = req?.body;
+
+      if (!isValid(id))
+        throw new Error("ID provided is not a valid document Id.");
+
+      const rentsDocument = await Rent.findOne({ ownerID: id });
+
+      if (!rentsDocument) throw new Error(rentsDocument);
+
+      const { rents } = rentsDocument;
+
+      const checkIfPropertyIdIsRegistered = rents[0][propertyId];
+
+      if (
+        !checkIfPropertyIdIsRegistered ||
+        (!checkIfPropertyIdIsRegistered &&
+          checkIfPropertyIdIsRegistered?.propertyNumber !== propertyNo)
+      ) {
+        if (
+          !checkIfPropertyIdIsRegistered &&
+          checkIfPropertyIdIsRegistered?.propertyNumber !== propertyNo
+        )
+          throw new Error(
+            "Property with the given property Id and  property number has not been registered in the tenants database."
+          );
+
+        if (!checkIfPropertyIdIsRegistered)
+          throw new Error(
+            "Property with the given property Id has not been registered in the tenants database."
+          );
+      }
+
+      const propertyRents = checkIfPropertyIdIsRegistered?.rentPayments;
+
+      if (!propertyRents)
+        throw new Error("No tenants have been added to this property.");
+
+      const checkIfRoomIdIsRegisteredUnderSelectedProperty =
+        propertyRents[roomId];
+
+      if (!checkIfRoomIdIsRegisteredUnderSelectedProperty)
+        throw new Error(
+          "Room with the given ID has not been registered in the tenants database."
+        );
+
+      const checkIfTenantIsRegisteredUnderSelectedRoomInSelectedProperty =
+        checkIfRoomIdIsRegisteredUnderSelectedProperty[tenantId];
+
+      if (!checkIfTenantIsRegisteredUnderSelectedRoomInSelectedProperty)
+        throw new Error(
+          "Tenant with the given ID has not been registered in the tenants database."
+        );
+
+      const newTenantPaymentReports =
+        checkIfTenantIsRegisteredUnderSelectedRoomInSelectedProperty.map(
+          (payment) => {
+            if (payment.paymentID !== paymentId) return payment;
+          }
+        );
+
+      if (!newTenantPaymentReports)
+        throw new Error(
+          "The requested payment report with the given payment ID was not found."
+        );
+
+      rents[0][propertyId].rentPayments[roomId][tenantId] =
+        newTenantPaymentReports;
+
+      const updateRents = await Rent.updateOne(
+        { ownerID: id },
+        {
+          $set: {
+            rents,
+          },
+        }
+      );
+
+      if (updateRents.acknowledged && updateRents.modifiedCount)
+        res.status(200).json({
+          message: `Rent payment for the room with ID: ${roomId} made by tenant with ID: ${tenantId} has been successfuly deleted.`,
+        });
+    } catch (err) {
+      if (err?.message) res.status(400).json({ error: err.message });
+    }
+  },
+};
+
+module.exports = patchControllers;
