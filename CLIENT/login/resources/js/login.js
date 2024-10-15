6f16acd53e5ae9893cbeadfa9d608b6d0efd0052
wawerusamuel <waweruzamuel@gmail.com>
2024-10-15 11:40:01 +0300

    added a demo login for testing...

---
+++ CLIENT/login/resources/js/login.js
@@ -0,0 +1,36 @@
+const loginForm = document.querySelector("form");
+
+loginForm.addEventListener("submit", async (e) => {
+  e.preventDefault();
+
+  const email = loginForm.querySelector("[data-email]");
+  const nationalID = loginForm.querySelector("[data-national-id]");
+  const password = loginForm.querySelector("[data-password]");
+
+  const loginRequestOptions = {
+    mode: "cors",
+    method: "POST",
+    headers: {
+      user: true,
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ email, nationalID, password }),
+  };
+
+  const loginRequest = await fetch(
+    "http://localhost:4000/api/user/owner/login",
+    loginRequestOptions
+  );
+
+  const { error, message, token } = await loginRequest.json();
+
+  if (error) {
+    alert(error);
+    return;
+  }
+
+  if (message && token) {
+    alert(message);
+    alert(token);
+  }
+});
