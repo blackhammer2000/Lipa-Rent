2f429bfd4c9d5866002f1a7d958fa5a68d9530f5
wawerusamuel <waweruzamuel@gmail.com>
2024-10-16 19:22:04 +0300

    working on rendering all properties..

--- CLIENT/dashboard/resources/js/dashboard.js
+++ CLIENT/dashboard/resources/js/dashboard.js
@@ -1,8 +1,16 @@
-const accessToken = localStorage.getItem("liparentAccessToken")
-  ? JSON.parse(localStorage.getItem("liparentAccessToken"))
-  : null;
+(async function () {
+  const accessToken = localStorage.getItem("liparentAccessToken")
+    ? JSON.parse(localStorage.getItem("liparentAccessToken"))
+    : null;
 
-if (accessToken === (null || undefined))
-  location.assign("/CLIENT/login/login.html");
+  if (accessToken === (null || undefined))
+    location.assign("/CLIENT/login/login.html");
 
-console.log(Store.readAllPropertiesOwned(accessToken));
+  const tableBody = document.querySelector("[data-table]");
+
+  const properties = await Store.readAllPropertiesOwned(accessToken);
+
+  if (!properties) return;
+
+  UserInterface.renderProperties(properties, tableBody);
+})();
