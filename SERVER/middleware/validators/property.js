7f52feb7f6d518e94efdeaf72813ce83e66e302b
wawerusamuel <waweruzamuel@gmail.com>
2024-10-20 18:04:02 +0300

    made changes to the property validator...

--- SERVER/middleware/validators/property.js
+++ SERVER/middleware/validators/property.js
@@ -3,19 +3,9 @@ const Joi = require("joi");
 const propertyValidator = async (req, res, next) => {
   try {
     const propertySchema = Joi.object({
+      propertyName: Joi.string().required().uppercase(),
       propertyNumber: Joi.string().required().uppercase(),
-      propertyTitleDetails: {
-        name: Joi.string().required(),
-        nationalID: Joi.string().required(),
-        asWho: Joi.string().required(),
-      },
       propertyLocation: Joi.string().required().uppercase(),
-      propertyValue: Joi.string().required(),
-      propertyPurpose: {
-        purposedUse: Joi.string().required(),
-        purposeType: Joi.string().required(),
-      },
-      isIdle: Joi.boolean().required(),
     });
 
     const isValidSignUpData = await propertySchema.validateAsync(
