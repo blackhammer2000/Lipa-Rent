b8bc8178d34904285ef7958c72fb827ef851a091
wawerusamuel <waweruzamuel@gmail.com>
2024-10-27 20:53:46 +0300

    made some changes...

--- CLIENT/pages/rooms/resources/js/classes/roomsclasses.js
+++ CLIENT/pages/rooms/resources/js/classes/roomsclasses.js
@@ -306,7 +306,7 @@ class UserInterface {
       this.updateTableDescription(propertyId);
       this.renderRooms(propertyRooms, accessToken, tableBody);
       this.clearFormInputs(form);
-      form.parentElement.parentElement.classList.add("hide");
+      form?.parentElement.parentElement.classList.add("hide");
       return;
     }
   }
