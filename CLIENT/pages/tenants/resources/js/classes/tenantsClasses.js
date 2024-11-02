36757ebd66d63acb41824279ecb0ad43ce4ae40e
wawerusamuel <waweruzamuel@gmail.com>
2024-11-02 18:37:52 +0300

    working on the rendering of the tenants data in the table...

--- CLIENT/pages/tenants/resources/js/classes/tenantsClasses.js
+++ CLIENT/pages/tenants/resources/js/classes/tenantsClasses.js
@@ -1,4 +1,32 @@
-class Store {}
+class Store {
+  static async readAllTenantsForRoomInProperty(
+    propertyId,
+    roomId,
+    accessToken
+  ) {
+    if (!propertyId || !roomId || !accessToken) return;
+
+    const requestOptions = {
+      mode: "cors",
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        user: true,
+        token: accessToken,
+      },
+      body: JSON.stringify({ propertyId, roomId }),
+    };
+
+    const readRoomTenantsRequest = await fetch(
+      "http://localhost:4000/api/user/owner/read/property/room/tenants",
+      requestOptions
+    );
+
+    const res = await readRoomTenantsRequest.json();
+
+    console.log(res);
+  }
+}
 class UserInterface {
   static renderTenants(tenants, accessToken, tableBody) {
     if (!accessToken || !tableBody) return;
---
+++ CLIENT/pages/tenants/resources/js/tenants.js
@@ -0,0 +1,14 @@
+(async function () {
+  const accessToken = localStorage.getItem("liparentAccessToken")
+    ? JSON.parse(localStorage.getItem("liparentAccessToken"))
+    : null;
+
+  if (accessToken === (null || undefined))
+    location.assign("/CLIENT/login/login.html");
+
+  Store.readAllTenantsForRoomInProperty(
+    "f61167454fa6",
+    "1bd1299235ff",
+    accessToken
+  );
+})();
