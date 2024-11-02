776ac53db46fd85487ab0b8b5e7eca112451995a
wawerusamuel <waweruzamuel@gmail.com>
2024-11-02 19:24:53 +0300

    created a mthod for creating the rows...

--- CLIENT/pages/tenants/resources/js/classes/tenantsClasses.js
+++ CLIENT/pages/tenants/resources/js/classes/tenantsClasses.js
@@ -54,5 +54,61 @@ class UserInterface extends UserinterfaceUtilities {
     tableBody.append(fragment);
   }
 
-  static createTenantRow(tenant, index, accessToken, tableBody) {}
+  static createTenantRow(tenant, index, accessToken, tableBody) {
+    if (!tenant || !index || !accessToken || !tableBody) return;
+
+    const { tenantID, tenantName, tenantNationalID, tenantPhone } = tenant;
+
+    const row = document.createElement("tr");
+
+    const tablenumberCell = document.createElement("td");
+    const tablenumberCellText = document.createTextNode(index);
+    tablenumberCell.append(tablenumberCellText);
+    row.append(tablenumberCell);
+
+    const tabTenantIdCell = document.createElement("td");
+    const tableTenantIdCellText = document.createTextNode(tenantID);
+    tabTenantIdCell.append(tableTenantIdCellText);
+    row.append(tabTenantIdCell);
+
+    const tableTenantNameCell = document.createElement("td");
+    const tableTenantNameCellText = document.createTextNode(tenantName);
+    tableTenantNameCell.append(tableTenantNameCellText);
+    row.append(tableTenantNameCell);
+
+    const tableTenantNationalIDCell = document.createElement("td");
+    const tableTenantNationalIDCellText =
+      document.createTextNode(tenantNationalID);
+    tableTenantNationalIDCell.append(tableTenantNationalIDCellText);
+    row.append(tableTenantNationalIDCell);
+
+    const tableTenantNumberCell = document.createElement("td");
+    const tableTenantNumberCellText = document.createTextNode(tenantPhone);
+    tableTenantNumberCell.append(tableTenantNumberCellText);
+    row.append(tableTenantNumberCell);
+
+    const rowCTAbuttonCell = document.createElement("td");
+
+    const editButtonCell = document.createElement("button");
+    editButtonCell.className = "btn btn-primary mr-2 edit";
+    const editButtonCellText = document.createTextNode("Edit");
+    editButtonCell.append(editButtonCellText);
+    // editButtonCell.addEventListener("click", (e) => {
+    //   this.populateEditRoomForm(e);
+    // });
+    rowCTAbuttonCell.append(editButtonCell);
+
+    const deleteButtonCell = document.createElement("button");
+    deleteButtonCell.className = "btn btn-danger ml-2 delete";
+    const deleteButtonCellText = document.createTextNode("Delete");
+    deleteButtonCell.append(deleteButtonCellText);
+    // deleteButtonCell.addEventListener("click", (e) => {
+    //   this.deleteRoomAndRender(e, accessToken, tableBody);
+    // });
+    rowCTAbuttonCell.append(deleteButtonCell);
+
+    row.append(rowCTAbuttonCell);
+
+    return row;
+  }
 }
