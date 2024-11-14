47a1214bbb5803c146c8904c33cdc2aa68aa43d2
wawerusamuel <waweruzamuel@gmail.com>
2024-11-14 23:23:34 +0300

    commented out the rooms sorter but will implement a better rooms sorter...

--- CLIENT/pages/rooms/resources/js/classes/roomsclasses.js
+++ CLIENT/pages/rooms/resources/js/classes/roomsclasses.js
@@ -205,15 +205,15 @@ class UserInterface extends UserinterfaceUtilities {
       return;
     }
 
-    const sortedRoomsByRoomNumber = Object.keys(rooms).sort(
-      (roomId1, roomId2) => {
-        if (rooms[roomId1].roomNumber > rooms[roomId2].roomNumber) return 1;
-        if (rooms[roomId1].roomNumber < rooms[roomId2].roomNumber) return -1;
-        if (rooms[roomId1].roomNumber === rooms[roomId2].roomNumber) return 0;
-      }
-    );
+    // const sortedRoomsByRoomNumber = Object.keys(rooms).sort(
+    //   (roomId1, roomId2) => {
+    //     if (rooms[roomId1].roomNumber > rooms[roomId2].roomNumber) return 1;
+    //     if (rooms[roomId1].roomNumber < rooms[roomId2].roomNumber) return -1;
+    //     if (rooms[roomId1].roomNumber === rooms[roomId2].roomNumber) return 0;
+    //   }
+    // );
 
-    for (const key of sortedRoomsByRoomNumber) {
+    for (const key in rooms) {
       const roomRow = this.createRoomRow(
         rooms[key],
         tableNumber,
