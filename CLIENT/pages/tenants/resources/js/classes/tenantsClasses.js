246e8f9ab8bd11203f4b4ae6401baef67e0488fe
wawerusamuel <waweruzamuel@gmail.com>
2024-11-16 23:23:11 +0300

    made some changes...

--- CLIENT/pages/tenants/resources/js/classes/tenantsClasses.js
+++ CLIENT/pages/tenants/resources/js/classes/tenantsClasses.js
@@ -186,7 +186,7 @@ class UserInterface extends UserinterfaceUtilities {
     const fragment = document.createDocumentFragment();
     let tableNumber = 1;
 
-    const reversedTenants = Object.keys(tenants).reverse();
+    const reversedTenants = Object.keys(tenants)?.reverse();
 
     for (const key of reversedTenants) {
       const roomRow = this.createTenantRow(
