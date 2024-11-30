4797356503a2f5dc60e32816c457bad38a5dd65c
wawerusamuel <waweruzamuel@gmail.com>
2024-11-30 18:10:27 +0300

    working on cretaing a section for the user to view their subscriptions...

---
+++ CLIENT/pages/subscriptions/resources/js/subscriptions.js
@@ -0,0 +1,8 @@
+(async function () {
+  const accessToken = localStorage.getItem("liparentAccessToken")
+    ? localStorage.getItem("liparentAccessToken")
+    : null;
+
+  if (accessToken === (null || undefined))
+    location.assign("/CLIENT/login/login.html");
+})();
