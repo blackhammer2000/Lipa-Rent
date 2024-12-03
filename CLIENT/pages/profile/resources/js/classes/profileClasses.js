5e41a6fb514255e4fe42eb4b8a50d58abbada9ab
wawerusamuel <waweruzamuel@gmail.com>
2024-12-03 16:51:45 +0300

    still working on the profile section...

--- CLIENT/pages/profile/resources/js/classes/profileClasses.js
+++ CLIENT/pages/profile/resources/js/classes/profileClasses.js
@@ -1,3 +1,62 @@
-class Store extends StoreUtilities {}
+class Store extends StoreUtilities {
+  static async readOwnerDetails(accessToken) {
+    if (!accessToken) return;
+
+    UserInterface.openLoader("reading owner details", "readOwner");
+
+    const requestOptions = {
+      mode: "cors",
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        user: true,
+        token: accessToken,
+      },
+    };
+
+    const readRoomTenantsRequest = await fetch(
+      "http://localhost:4000/api/user/owner/read/owner",
+      requestOptions
+    );
+
+    const { owner, error } = await readRoomTenantsRequest.json();
+
+    if (owner || error) UserInterface.closeLoader("readOwner");
+
+    if (error) UserInterface.handleErrors(error);
+
+    if (owner) return { owner };
+  }
+
+  static async editOwnerDetails(accessToken) {
+    if (!accessToken) return;
+
+    UserInterface.openLoader("editing owner details", "editOwner");
+
+    const requestOptions = {
+      mode: "cors",
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+        user: true,
+        token: accessToken,
+      },
+      body: JSON.stringify(editedOwnerDetails),
+    };
+
+    const readRoomTenantsRequest = await fetch(
+      "http://localhost:4000/api/user/owner/edit/owner",
+      requestOptions
+    );
+
+    const { owner, error } = await readRoomTenantsRequest.json();
+
+    if (owner || error) UserInterface.closeLoader("readUser");
+
+    if (error) UserInterface.handleErrors(error);
+
+    if (owner) return { owner };
+  }
+}
 
 class UserInterface extends UserinterfaceUtilities {}
