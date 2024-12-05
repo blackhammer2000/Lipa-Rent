cd8b2c8e7daf3bfa3c607d461df3c80df58b4a65
wawerusamuel <waweruzamuel@gmail.com>
2024-12-05 20:32:27 +0300

    started working on the delete owner functionality...

--- SERVER/routes/users/controllers/deleteControllers.js
+++ SERVER/routes/users/controllers/deleteControllers.js
@@ -15,17 +15,13 @@ const patchControllers = {
     try {
       if (!req.body.id)
         throw new Error("Unauthorized action, not a user or not logged in.");
-      if (!req.body.editedOwner)
-        throw new Error("Provide valid edited owner details.");
 
-      const { id, editedOwner } = req.body;
+      const { id } = req.body;
 
       if (!isValid(id))
         throw new Error("ID provided is not a valid document Id.");
 
-      const { name, nationalID } = editedOwner;
-
-      const ownerPropertiesDocument = await Owner.findOne({
+      const ownerPropertiesDocument = await Owner.findOneAndDelete({
         _id: id.toString(),
       });
 
