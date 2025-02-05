532f1cfba64bbe006612c07f1aa83337fcb40805
wawerusamuel <waweruzamuel@gmail.com>
2025-02-05 23:16:06 +0300

    created a module for generating an access token for the admin...

---
+++ SERVER/middleware/tokens/adminAccessToken.js
@@ -0,0 +1,17 @@
+const { sign } = require("jsonwebtoken");
+require("dotenv").config();
+
+const signAdminAccessToken = (adminData) => {
+  return new Promise((resolve, reject) => {
+    const token = sign(adminData, process.env.MY_SECRET_KEY_ADMIN, {
+      expiresIn: "2h",
+      issuer: "liparent inc.",
+      audience: `${adminData}`,
+    });
+
+    if (!token) reject(token);
+    resolve(token);
+  });
+};
+
+module.exports = { signAdminAccessToken };
