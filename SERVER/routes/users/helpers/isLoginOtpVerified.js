82d98ad0c38e802e0813696d78dc7b42141f2b26
wawerusamuel <waweruzamuel@gmail.com>
2025-04-14 18:23:36 +0300

    created a middleware to make sure that the user successfully logs in before any request is made......

---
+++ SERVER/routes/users/helpers/isLoginOtpVerified.js
@@ -0,0 +1,25 @@
+async function isLoginOtpVerified(req, res, next) {
+  try {
+    if (!req.body.id) throw new Error("Unauthorized action.");
+
+    const {
+      body: { id },
+    } = req;
+
+    const userOtpDoc = await Otp.findOne({ ownerID: id });
+
+    const isLoginOtpVerified = userOtpDoc.isLoginOtpVerified || null;
+
+    if (
+      isLoginOtpVerified === false ||
+      isLoginOtpVerified === (null || undefined)
+    )
+      throw new Error("Invalid login sesion");
+
+    next();
+  } catch (err) {
+    if (err?.message) res.status(500).json({ error: err?.message });
+  }
+}
+
+module.exports = { isLoginOtpVerified };
