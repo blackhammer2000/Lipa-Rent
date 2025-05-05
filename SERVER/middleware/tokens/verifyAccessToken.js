ad26ef9f1b342dd574426b9af5baf1f0ddac8569
wawerusamuel <waweruzamuel@gmail.com>
2025-05-05 19:10:23 +0300

    removed some double logic that would cause a bug if the upon token expiry...

--- SERVER/middleware/tokens/verifyAccessToken.js
+++ SERVER/middleware/tokens/verifyAccessToken.js
@@ -46,11 +46,6 @@ const verifyUserAccessToken = async (req, res, next) => {
     if (
       err?.message === ("jwt expired" || "invalid token" || "jwt malformed")
     ) {
-      const otpDocDeletion = await Otp.deleteOne({ ownerID: id });
-
-      if (!otpDocDeletion.acknowledged && !otpDocDeletion.deletedCount)
-        throw new Error("something went wrong, 'otp'");
-
       res.status(403).json({ error: "session expired" });
     } else {
       res.status(500).json({ error: err?.message });
