f8b8f574849eab4da7999ed04fa3c0a3947deeb3
wawerusamuel <waweruzamuel@gmail.com>
2025-05-15 17:59:32 +0300

    removed the admin server folder as the admin server will be seperate with the users server...

--- SERVER/routes/admin/routes/post.js
+++
@@ -1,8 +0,0 @@
-const router = require("express").Router();
-
-const { login } = require("../controllers/postControllers");
-
-router.post("/api/admin/login", isAdmin, login);
-router.post();
-
-module.exports = router;
