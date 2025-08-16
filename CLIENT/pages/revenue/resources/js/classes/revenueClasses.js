92718414b94f868d00fff796a82a9600b2a675b9
wawerusamuel <waweruzamuel@gmail.com>
2025-08-16 19:18:38 +0300

    made some changes...

--- CLIENT/pages/revenue/resources/js/classes/revenueClasses.js
+++ CLIENT/pages/revenue/resources/js/classes/revenueClasses.js
@@ -137,7 +137,7 @@ class UserInterface extends UserinterfaceUtilities {
       return;
     }
 
-    console.log(selectedRangePayments);
+    // console.log(selectedRangePayments);
 
     // const selectedRangeTotalRevenue = selectedRangePayments.reduce(
     //   (a, b) => a.amountPaid + b.amountPaid
