34fd6a3cd93b08148526b50fc2d7cd6d145f2fe3
wawerusam <waweruzamuel@gmail.com>
2026-08-19 21:05:15 +0300

    removed unwanted element...

--- CLIENT/react/src/pages/Landing.jsx
+++ CLIENT/react/src/pages/Landing.jsx
@@ -291,7 +291,6 @@ export default function Landing() {
                 required
               />
             </div>
-            {error && <div className="alert alert-danger">{error}</div>}
             <div className="form-group text-center">
               <button type="submit" className="btn btn-success w-100">
                 Verify
